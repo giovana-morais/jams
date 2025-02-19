a3d0ce722f402ffb4d1018617d18d9d78b97dbed
Giovana Morais <giovana.vmorais@gmail.com>
2025-02-18 20:24:31 -0500

    test rollback

--- jams/__init__.py
+++ jams/__init__.py
@@ -2,8 +2,9 @@
 """Top-level module for JAMS"""
 
 import os
-from importlib import resources
-from itertools import chain
+# from importlib import resources
+# from itertools import chain
+from pkg_resources import resource_filename
 
 # Import the necessary modules
 from .exceptions import *
@@ -20,24 +21,17 @@ from .schema import list_namespaces
 
 # # Populate the namespace mapping
 # for ns in chain(*map(lambda p: p.rglob('*.json'), resources.files('jams.schemata.namespaces').iterdir())):
+#     schema.add_namespace(ns)
 
-# Ensure that the package exists and contains the expected resources
-try:
-    resource_path = resources.files('jams.schemata.namespaces')
-    if resource_path is None:
-        raise ValueError("Resource path is None")
-
-    # Iterate over the directories and search for JSON files
-    json_files = chain(*map(lambda p: p.rglob('*.json'), resource_path.iterdir()))
-
-    # Check if any JSON files were found
-    for ns in json_files:
-        schema.add_namespace(ns)
-except Exception as e:
-    print(f"An error occurred: {e}")
+# Populate the namespace mapping
+for _ in util.find_with_extension(resource_filename(__name__, schema.NS_SCHEMA_DIR),
+                                  'json'):
+    schema.add_namespace(_)
 
 # Populate local namespaces
-
 if 'JAMS_SCHEMA_DIR' in os.environ:
-    for ns in util.find_with_extension(os.environ['JAMS_SCHEMA_DIR'], 'json'):
-        schema.add_namespace(ns)
+    try:
+        for ns in util.find_with_extension(os.environ['JAMS_SCHEMA_DIR'], 'json'):
+            schema.add_namespace(ns)
+    except KeyError:
+        pass
